a4b3c305f01f8c2b9b60ef660d391926c9af5d03
Arohi Mehta <mehtaarohi@gmail.com>
2025-04-02 23:07:39 -0400

    got login and database functionality working and user able to add friends

---
+++ Mobile/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  Dear Lampi
+//
+//  Created by Arohi Mehta on 4/2/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
